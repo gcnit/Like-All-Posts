e3df2d461b88d06bd93a47abe4007d3f2ceb58b1
Gaurav <gaurav01101993@gmail.com>
2014-10-18 10:39:37 +0530

    v1.0

---
+++ Like All Posts.py
@@ -0,0 +1,22 @@
+import requests
+import json
+import time
+print "Welcome to Like All Posts"
+Token = raw_input("\nGo to https://developers.facebook.com/tools/explorer ,\nclick on get access token,\ncheck on all the permissions in basic and extended permissions \nand paste the access token here\n")
+PAGEID = raw_input("\nEnter the numeric page_id/user_id, you can get it in the URL of the page/user.Ex:www.facebook.com/pageid\n")
+print "\nPlease wait while all the posts by " + PAGEID + " is being liked\n"
+
+def like():
+    query = ("SELECT post_id FROM stream WHERE "
+            "source_id ="+PAGEID+"  AND actor_id="+PAGEID+" LIMIT 200")
+    payload = {'q': query, 'access_token': Token}
+    r = requests.get('https://graph.facebook.com/fql', params=payload)
+    result = json.loads(r.text)
+    for wallpost in result['data']:
+        requests.post("https://graph.facebook.com/" + str(wallpost['post_id'])
+                      + "/likes/?access_token=" + Token
+                      + "&method=POST")        
+    print "\nAll posts liked!"
+
+if __name__ == '__main__':
+    like()
